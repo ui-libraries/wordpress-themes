8ca544e339ec1b5f9c7cf9ce37fa6daf8885f1f2
JoeyWroble <jwroble@uiowa.edu>
2024-02-15 14:28:48 -0600

    Completely removed site info
    Just removing "Powered by WordPress" text created an accessibility issue (because the link was no longer visible), so i removed the link that was attached to the text too

--- geometries-twentyseventeen-child/template-parts/footer/site-info.php
+++ geometries-twentyseventeen-child/template-parts/footer/site-info.php
@@ -7,14 +7,3 @@
  * @since Twenty Seventeen 1.0
  * @version 1.0
  */
-
-?>
-<div class="site-info">
-	<?php
-	if ( function_exists( 'the_privacy_policy_link' ) ) {
-		the_privacy_policy_link( '', '<span role="separator" aria-hidden="true"></span>' );
-	}
-	?>
-	<a href="<?php echo esc_url( __( 'https://wordpress.org/', 'twentyseventeen' ) ); ?>" class="imprint">
-	</a>
-</div><!-- .site-info -->
